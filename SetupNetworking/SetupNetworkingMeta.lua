f59156c6bc17844455ff26717a6c01ece44cdb5f
Will Szumski <will@cowboycoders.org>
2012-01-24 15:06:49 +0000

    FIXME: Bodged licenses

--- SetupNetworking/SetupNetworkingMeta.lua
+++ SetupNetworking/SetupNetworkingMeta.lua
@@ -1,4 +1,23 @@
 
+    --[[
+    A frontend to wicd for squeezeplay
+    Copyright (C) 2011  Will Szumski
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    --]]
+
+
 --[[
 =head1 NAME
 
@@ -49,14 +68,5 @@ function registerApplet(meta)
 end
 
 
---[[
-
-=head1 LICENSE
-
-This source code is public domain. It is intended for you to use as a starting
-point to create your own applet.
-
-=cut
---]]
 
 
